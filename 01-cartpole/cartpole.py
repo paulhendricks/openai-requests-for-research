502dd5cff0386db86b3a95f6f7621e7a802a0af5
paulhendricks <paul.hendricks.2013@owu.edu>
2016-10-31 14:27:56 -0400

    added blind search algorithms

--- 01-cartpole/cartpole.py
+++ 01-cartpole/cartpole.py
@@ -1,5 +1,5 @@
 import gym
-from playground.algos.linear import LinearAgent
+from playground.algos.blind import GridSearchAgent, MonteCarloAgent
 from playground.algos.local import HillClimbingAgent, SimulatedAnnealingAgent
 from playground.algos.random import RandomAgent
 from playground.experiment import run_experiment
@@ -8,17 +8,21 @@ from playground.experiment import run_experiment
 env = gym.make('CartPole-v0')
 
 # Run random agent
-agent = RandomAgent(env.action_space)
+agent = RandomAgent(env.observation_space, env.action_space)
 run_experiment(env, agent)
 
-# Run linear agent
-agent = LinearAgent(env.action_space)
+# Run grid search agent
+agent = GridSearchAgent(env.observation_space, env.action_space)
+run_experiment(env, agent)
+
+# Run monte carlo agent
+agent = MonteCarloAgent(env.observation_space, env.action_space)
 run_experiment(env, agent)
 
 # Run hill climbing agent
-agent = HillClimbingAgent(env.action_space)
+agent = HillClimbingAgent(env.observation_space, env.action_space)
 run_experiment(env, agent)
 
 # Run simulated annealing agent
-agent = SimulatedAnnealingAgent(env.action_space)
+agent = SimulatedAnnealingAgent(env.observation_space, env.action_space)
 run_experiment(env, agent)
