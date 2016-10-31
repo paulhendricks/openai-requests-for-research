7d84dd50c69873eb3b2713473432ad129fb3a2e2
paulhendricks <paul.hendricks.2013@owu.edu>
2016-10-31 16:01:03 -0400

    updaed example with better args

--- 01-cartpole/cartpole.py
+++ 01-cartpole/cartpole.py
@@ -8,8 +8,8 @@ env = gym.make('CartPole-v0')
 
 # Run random guessing (a.k.a. monte carlo) agent
 agent = MonteCarloAgent(env.observation_space, env.action_space)
-run_experiment(env, agent)
+result = run_experiment(env, agent, episode_count=10000, watch=False)
 
 # Run hill climbing agent
 agent = HillClimbingAgent(env.observation_space, env.action_space)
-run_experiment(env, agent)
+result = run_experiment(env, agent)
