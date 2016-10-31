d76b628a2a5241306b1e87d62fd0fd268041184b
paulhendricks <paul.hendricks.2013@owu.edu>
2016-10-31 14:49:01 -0400

    updated example

--- 01-cartpole/cartpole.py
+++ 01-cartpole/cartpole.py
@@ -1,28 +1,15 @@
 import gym
-from playground.algos.blind import GridSearchAgent, MonteCarloAgent
-from playground.algos.local import HillClimbingAgent, SimulatedAnnealingAgent
-from playground.algos.random import RandomAgent
+from playground.algos.blind import MonteCarloAgent
+from playground.algos.local import HillClimbingAgent
 from playground.experiment import run_experiment
 
 # Make environment
 env = gym.make('CartPole-v0')
 
-# Run random agent
-agent = RandomAgent(env.observation_space, env.action_space)
-run_experiment(env, agent)
-
-# Run grid search agent
-agent = GridSearchAgent(env.observation_space, env.action_space)
-run_experiment(env, agent)
-
-# Run monte carlo agent
+# Run random guessing (a.k.a. monte carlo) agent
 agent = MonteCarloAgent(env.observation_space, env.action_space)
 run_experiment(env, agent)
 
 # Run hill climbing agent
 agent = HillClimbingAgent(env.observation_space, env.action_space)
-run_experiment(env, agent)
-
-# Run simulated annealing agent
-agent = SimulatedAnnealingAgent(env.observation_space, env.action_space)
 run_experiment(env, agent)
