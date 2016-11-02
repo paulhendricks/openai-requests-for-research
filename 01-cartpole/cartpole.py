b9a846bdd5b525a598769f2b3f1d5cd7e3742c96
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-02 09:50:07 -0400

    updated examples with policy gradient agent

--- 01-cartpole/cartpole.py
+++ 01-cartpole/cartpole.py
@@ -1,5 +1,6 @@
 import gym
 from playground.algos.blind import MonteCarloAgent
+from playground.algos.gradient import PolicyGradientAgent
 from playground.algos.local import HillClimbingAgent
 from playground.experiment import run_experiment
 
@@ -9,7 +10,14 @@ env = gym.make('CartPole-v0')
 # Run random guessing (a.k.a. monte carlo) agent
 agent = MonteCarloAgent(env.observation_space, env.action_space)
 result = run_experiment(env, agent, episode_count=10000, watch=False)
+run_experiment(env, result)
 
 # Run hill climbing agent
 agent = HillClimbingAgent(env.observation_space, env.action_space)
 result = run_experiment(env, agent, episode_count=5000, watch=False)
+run_experiment(env, result)
+
+# Run policy gradient agent
+agent = PolicyGradientAgent(env.observation_space, env.action_space)
+result = run_experiment(env, agent, episode_count=5000, watch=False)
+run_experiment(env, result)
