b1b073da9b1e43d67f0b8b21e459e0e23bda3679
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-02 17:11:30 -0400

    updated with policy gradient agent

--- 01-cartpole/cartpole.py
+++ 01-cartpole/cartpole.py
@@ -3,6 +3,7 @@ from playground.algos.blind import MonteCarloAgent
 from playground.algos.gradient import PolicyGradientAgent
 from playground.algos.local import HillClimbingAgent
 from playground.experiment import run_experiment
+import tensorflow as tf
 
 # Make environment
 env = gym.make('CartPole-v0')
@@ -19,5 +20,9 @@ run_experiment(env, result)
 
 # Run policy gradient agent
 agent = PolicyGradientAgent(env.observation_space, env.action_space)
-result = run_experiment(env, agent, episode_count=5000, watch=False)
-run_experiment(env, result)
+policy_grad = agent.policy_gradient()
+value_grad = agent.value_gradient()
+sess = tf.InteractiveSession()
+sess.run(tf.initialize_all_variables())
+for _ in range(2000):
+    reward = agent.run_episode(env, policy_grad, value_grad, sess)
